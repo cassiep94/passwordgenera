e08ef74d4d142a7348fde8f98e4da980dd36e173
Cassandra Perez <cassandraenriq@icloud.com>
2020-04-17 18:07:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// var pTags = document.querySelectorAll("p");
+// var divTags = document.querySelectorAll("div");
+// var aTags = document.querySelectorAll("a");
+// var imgEl = document.querySelectorAll("img");
+// var changeP = document.querySelector("#change2");
+
+// console.log(pTags);
