58d78d906dd2eaf20441916fce454404390b0fc3
Cassandra Perez <cassandraenriq@icloud.com>
2020-04-18 18:06:22 -0500

    functions

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+/**
+ * These are five functions that are promting the user for infomation.
+ * They are returning true or false debepeding on what the user is clicking. 
+ */
+function getPasswordLenght() {
+  return prompt("Enter your password lenght between 8-128 characters");
+}
+function includeLowerCaseinPassword() {
+  return confirm("Do you want lowercase characters in your password?");
+}
+function includeUpperCaseinPassword() {
+  return confirm("Do you want uppercase characters in your password?");
+}
+function includeNumbersinPassword() {
+  return confirm("Do you want numbers in your password?");
+}
+function includeSpecialCharinPassword() {
+  return confirm("Do you want special charaters in your password?");
+}
